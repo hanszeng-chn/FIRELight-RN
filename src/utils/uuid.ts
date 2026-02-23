f696396531bef5b4cfeb441ae498e817c6451bf3
Hans Zeng <346348376@qq.com>
2026-02-23 22:15:39 +0800

    重构(工具): 使用 expo-crypto 替换 uuid

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -2,12 +2,12 @@
  * UUID 生成工具
  */
 
-import { v4 as uuidv4 } from 'uuid';
+import * as Crypto from "expo-crypto";
 
 /**
  * 生成 UUID v4
  * @returns 唯一标识符字符串
  */
 export const generateUUID = (): string => {
-  return uuidv4();
+  return Crypto.randomUUID();
 };
